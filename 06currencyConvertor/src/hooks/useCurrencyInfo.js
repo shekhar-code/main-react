bf3ab302f588188f163276b8581f42f1b50bfe90
shekhar bansal <bansalshekhar2002@gmail.com>
2024-04-11 08:32:05 +0530

    currencychanger

---
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useState , useEffect } from "react";
+
+function useCurrencyInfo(currency){
+
+  const [data , setData] = useState({})
+
+  useEffect( () => {
+
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-02/v1/currencies/${currency}.json`)
+    .then( (res) => res.json())
+    .then( (res) => setData(res[currency]))
+    .catch( (err) => {console.log(err);})
+
+  } , [currency])
+  console.log(data);
+  return data
+
+}
+
+export default useCurrencyInfo;
