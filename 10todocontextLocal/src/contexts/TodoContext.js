323e7e82d7841e38f300c134fbe157fadfcc4510
shekhar bansal <bansalshekhar2002@gmail.com>
2024-04-12 08:18:54 +0530

    todo mini project

---
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+
+import {useContext , createContext} from 'react'
+
+const TodoContext = createContext({
+    todos: [ {
+      id: 1,
+      todo: "msg",
+      completed: false
+    }],
+    addTodo: (todo) => {},
+    updateTodo: (id , todo) => {},
+    deleteTodo: (id) => {},
+    toogleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+  return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider;
