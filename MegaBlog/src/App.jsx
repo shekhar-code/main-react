2493c861f1dfa1b2cf21ff3b35cd6ad3fcbb527e
shekhar bansal <bansalshekhar2002@gmail.com>
2024-04-13 11:55:46 +0530

    create store and components

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,14 +1,43 @@
 
+import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+import authService from './appwrite/auth'
+import {useDispatch} from 'react-redux'
+import {login , logout} from './store/authSlice'
+import {Header , Footer} from './components'
 
 function App() {
   // const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>mega blog</h1>
-    </>
-  )
+  useEffect(() => {
+
+    authService.getCurrentUser()
+    .then( (userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        }
+        else {
+          dispatch(logout())
+        }
+    } )
+    .finally( () => setLoading(false))
+  }, [])
+  
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         TODO:  {/*<Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
---
+++ MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+/*eslint-disable no-unused-vars*/
+
+function Logo({width = '100px'}) {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
